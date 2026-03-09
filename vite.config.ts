c8d6cf70bda0cb98f87db644a477634c36f1fc11
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-03-08 17:47:31 -0700

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     plugins: [
         solidPlugin(),
     ],
+    server: {
+        port: 3000,
+    },
     build: {
         target: "esnext",
         rollupOptions: {
